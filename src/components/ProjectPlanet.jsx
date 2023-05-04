2d20d1fbc07c16f0d6d893c3b714906638a2f206
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-05-05 00:14:53 +0100

    adding project planet

---
+++ src/components/ProjectPlanet.jsx
@@ -0,0 +1,23 @@
+import planet1 from '../assets/img/planets/planet 1.png'
+import planet2 from '../assets/img/planets/planet 2.png'
+import planet3 from '../assets/img/planets/planet 3.png'
+import planet4 from '../assets/img/planets/planet 4.png'
+import planet5 from '../assets/img/planets/planet 5.png'
+import { Link } from 'react-router-dom'
+
+export const ProjectPlanet = ({ index, id, title, shortDescription, description, tools, time }) => {
+    const planets = [planet1, planet3, planet2, planet4, planet5]
+    console.log(id)
+
+    return (
+        <Link to={`/projects/${id}`} className='flex py-4 font-SpaceMono mx-8' >
+            <img src={planets[index % planets.length]} alt="" className='px-2 spin'  />
+            <div className='px-2 border-l-2 flex-shrink-0'>
+                <h2 className='text-lg md:text-xl uppercase'>{title}</h2>
+                <h3 className='text-lg'>{shortDescription}</h3>
+                <h3 className=''>{tools}</h3>
+                <h3 className=''>{time}</h3>  
+            </div>
+        </Link>
+    );
+};
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,20 +1,34 @@
 import React from 'react'
 import rocket from '../assets/img/Rocketship.png'
 import settingAstronaut from '../assets/img/setting astronaute 200.png'
+import projectsData from "../data/projects.json";
+import { ProjectPlanet } from './ProjectPlanet';
 
 
 
- function Projects() {
+function Projects() {
+  const resolvedProjects = projectsData.projects
   return (
     <div className='text-white h-screen font-SpaceMono flex flex-col' id='projects'>
       <div className='flex-none flex h-10 justify-center pt-4'>
         <h1 className=' text-2xl 2xl:text-3xl uppercase'>My Projects </h1>
         <img src={rocket} alt="" />
       </div>
-      <div className='flex flex-grow mt-10'>
-        <img src={settingAstronaut} alt="" className='ml-12 animate-bounce h-1/2'/>
+      <div className='md:flex md:flex-grow mt-10'>
+        <img src={settingAstronaut} alt="" className='ml-12 px-4 animate-bounce h-1/2' />
+        <div className='px-8 flex flex-wrap'>
+          {resolvedProjects.map((project, index) => {
+            return (
+              <div key={index} className="w-full md:w-1/2 lg:w-1/3 mb-4 px-2">
+                <ProjectPlanet 
+                index = {index}
+                {...project} />
+              </div>
+            )
+          })}
+        </div>
       </div>
-      
+
     </div>
   )
 }
